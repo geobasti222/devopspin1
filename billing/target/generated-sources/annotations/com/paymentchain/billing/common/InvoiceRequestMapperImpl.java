ac6c9ac0cfbf20d021aa557320e067d046a47794
Geo <geobasti222@gmail.com>
2024-06-25 22:11:42 -0500

    i

---
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -0,0 +1,47 @@
+package com.paymentchain.billing.common;
+
+import com.paymentchain.billing.dto.InvoiceRequest;
+import com.paymentchain.billing.entities.Invoice;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-06-25T22:00:50-0500",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240620-1855, environment: Java 17.0.11 (Eclipse Adoptium)"
+)
+@Component
+public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
+
+    @Override
+    public Invoice InvoiceRequestToInvoice(InvoiceRequest source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        Invoice invoice = new Invoice();
+
+        invoice.setCustomerId( source.getCustomer() );
+        invoice.setAmount( source.getAmount() );
+        invoice.setDetail( source.getDetail() );
+        invoice.setNumber( source.getNumber() );
+
+        return invoice;
+    }
+
+    @Override
+    public List<Invoice> InvoiceRequestListToInvoiceList(List<InvoiceRequest> source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        List<Invoice> list = new ArrayList<Invoice>( source.size() );
+        for ( InvoiceRequest invoiceRequest : source ) {
+            list.add( InvoiceRequestToInvoice( invoiceRequest ) );
+        }
+
+        return list;
+    }
+}
